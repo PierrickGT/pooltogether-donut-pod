fd31a547a8ad1a69a52f015e71ff9feae6a6a06f
Pierrick Turelier <pturelier@meilleursagents.com>
2020-05-28 03:26:47 +0200

    chore(clean): remove unused files

--- packages/react-app/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Account } from './Account';
-export { default as Address } from './Address';
-export { default as Balance } from './Balance';
-export { default as Faucet } from './Faucet';
-export { default as Header } from './Header';
-export { default as Provider } from './Provider';
-export { default as Ramp } from './Ramp';
-export { default as Timeline } from './Timeline';
