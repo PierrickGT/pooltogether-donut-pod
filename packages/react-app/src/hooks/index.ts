9dfa52f219902a64f5444ba6e965cced12935cc4
Pierrick Turelier <pturelier@meilleursagents.com>
2020-05-28 02:06:43 +0200

    chore(clean): remove unuse code

--- packages/react-app/src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as useBalance } from './Balance';
-export { default as useBlockNumber } from './BlockNumber';
-// export { default as useContractLoader } from './ContractLoader';
-export { default as useContractReader } from './ContractReader';
-export { default as useEventListener } from './EventListener';
-export { default as useExchangePrice } from './ExchangePrice';
-export { default as useGasPrice } from './GasPrice';
-export { default as usePoller } from './Poller';
